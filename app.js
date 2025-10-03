40ab119e1fb4fbd3543bd6986e5e5d5c222c2e95
Sherone <keynessherone@gmail.com>
2025-10-03 22:02:23 +0530

    initail commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+// middlewares
+const methodOverride = require("method-override");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// set engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("HomePage")
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("oops something went wrong", error);
+    return;
+  }
+  console.log("Server listening in port: ", port);
+});
