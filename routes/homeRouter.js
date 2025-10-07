b1b34ef41f46e54f52289c374f54532c76bc42ef
Sherone <keynessherone@gmail.com>
2025-10-07 23:21:53 +0530

    complete login logout

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -4,15 +4,31 @@ const signupController = require("../controllers/signupController.js");
 const loginController = require("../controllers/loginController.js");
 
 router.get("/", (req, res) => {
+  console.log(req.user);
   res.render("HomePage");
 });
 
-router.get("/signup", signupController.showSignupForm);
+function redirectIfAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+}
+
+router.get("/signup", redirectIfAuthenticated, signupController.showSignupForm);
 
 router.post("/signup", signupController.registerUser);
 
-router.get("/login", loginController.showLoginForm);
+router.get("/login", redirectIfAuthenticated, loginController.showLoginForm);
 
 router.post("/login", loginController.checkUserDetail);
+
+router.post("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
