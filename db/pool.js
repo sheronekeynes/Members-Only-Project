6072789604667b153579c463eacd14ae3d2ba4eb
sherone <keynessherone@gmail.com>
2025-10-11 16:38:42 +0530

    deployement

--- db/pool.js
+++ db/pool.js
@@ -2,12 +2,19 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
+// const pool = new Pool({
+//   host: process.env.PGHOST,
+//   user: process.env.PGUSER,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
